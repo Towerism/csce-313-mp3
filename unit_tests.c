2ffb48dfcdd2f17bfa9c293aceecf095001eb16f
Martin <martin.frackerjr@gmail.com>
2015-03-08 19:20:31 -0500

    Uncommented lines of code.

--- unit_tests.c
+++ unit_tests.c
@@ -6,9 +6,9 @@
 #include "linked_list.h"
 
 int main() {
-  //if an assert fails, it'll print and throw an abort
-  //assert(test_init() == 1);
-  //assert(test_release() == 1);
+  // if an assert fails, it'll print and throw an abort
+  assert(test_init() == 1);
+  assert(test_release() == 1);
   assert(test_list() == 1);
   printf("Unit Tests have been run successfully\n");
   return 1;
