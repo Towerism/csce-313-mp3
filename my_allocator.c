4793f5c52d6f6fb3f22ef0dbb76bc9a239fcb97c
Christopher Findeisen <cfindeisen7@gmail.com>
2015-03-11 12:11:32 -0500

    changes to myalloc stashed.

--- my_allocator.c
+++ my_allocator.c
@@ -52,49 +52,56 @@ void * memory_pool = NULL;
 unsigned int b_b_s;
 void * list_offset;
 int high_order = 1;
+struct Linked_list* free_lists[8];
 //big-endian. That is..the largest LL is stored first.
 void * free_list_allocator( int total_mem_request ){
-  int offset = 4;//some num that = the size of the linked list
+  //linked List + all the nodes.
 //log takes a double but we want it rounded to an int
   high_order = log2((double) total_mem_request / b_b_s);
   int remaining_mem = total_mem_request;
+
+  int offset = 0;//some num that = the size of the linked list
+
   for(int order = high_order; order > 0; --order){
-    struct Linked_list* ll = (memory_pool + offset);
-    *ll = list_create();
-    ll->size = pow(2, order) * b_b_s;
-    //allocate as many high-order blocks as possible
-    while(remaining_mem >= ll->size){
-        list_push_back(ll, memory_pool + remaining_mem);
-        remaining_mem -= ll->size;
-    }
-    offset += sizeof(struct Linked_list);
-  }
-  return memory_pool + offset;
-}
+    offset += sizeof(struct Linked_list) ;
 
-unsigned int init_allocator(unsigned int _basic_block_size, unsigned int _length){
-  //reset error flag
-  errno = 0;
+    *free_lists[order] = list_create(pow(2,order) * b_b_s, 10);
+    struct Linked_list * ll = free_lists[order];
+        ll->block_size = pow(2, order) * b_b_s;
+        //allocate as many high-order blocks as possible
+        while(remaining_mem >= ll->block_size){
+        offset += (sizeof(struct Node);
+        list_insert(ll, , ); //struct Linked_list* list, struct Node* node, Addr ptr, int len
+        remaining_mem -= ll->block_size;
+        }
+        offset += sizeof(struct Linked_list);
+        }
+        return memory_pool + offset;
+        }
 
-  if(_length < _basic_block_size || _basic_block_size == 0){
-    printf("Provide sane input please. \n");
-    return 0;
-  }
-  if(memory_pool != NULL){
-    printf("You must release the memory you already allocated \n");
-    return 0;
-  }
+        unsigned int init_allocator(unsigned int _basic_block_size, unsigned int _length){
+        //reset error flag
+        errno = 0;
 
-  memory_pool = sbrk(_length);
-  b_b_s = _basic_block_size;
-  if(errno)
-    return 0;
+        if(_length < _basic_block_size || _basic_block_size == 0){
+        printf("Provide sane input please. \n");
+        return 0;
+        }
+        if(memory_pool != NULL){
+          printf("You must release the memory you already allocated \n");
+          return 0;
+        }
 
-  else {
-    list_offset = free_list_allocator(_length);
-    return _length;
-  }
-}
+        memory_pool = sbrk(_length);
+        b_b_s = _basic_block_size;
+        if(errno)
+          return 0;
+
+        else {
+          list_offset = free_list_allocator(_length);
+          return _length;
+        }
+        }
 int release_allocator(){
   if(memory_pool == NULL) {
     return 0;
@@ -123,6 +130,12 @@ Addr findBuddy(Addr x, int size){
   }
 }
 
+
+struct Linked_list* getList(int order){
+  struct Linked_list* point_list* = list_create(5,5);
+  return point_list;
+}
+
 Addr split(unsigned int order){
   //C error handling is nonexistent(almost) this will abort if we run out of memory..
   if(order > high_order){
@@ -133,7 +146,7 @@ Addr split(unsigned int order){
   struct Linked_list* free_list = getList(order);
   struct Linked_list* lower_free_list = getList(order-1);
 
-  if(free_List->empty()){
+  if(free_list->head == NULL){
     split(order+1);
   }
 
@@ -147,13 +160,12 @@ Addr split(unsigned int order){
 }
 
 
-
 extern Addr my_malloc(unsigned int _length) {
   double exact = log2((double) _length / b_b_s) ;
   int order = ceil(exact);
   Addr free_block = NULL;
   struct Linked_list* free_list = getList(order);
-  if(free_List->empty())
+  if(free_list->head == NULL)
     free_block = split(order);
   else {
     free_block = list_pop_back(free_list);
