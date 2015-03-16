7a37c1452a0592c241f57615bd6a71e147f0046c
Christopher Findeisen <cfindeisen7@gmail.com>
2015-03-15 23:04:42 -0500

    changed size of allocation

--- memtest.c
+++ memtest.c
@@ -4,7 +4,7 @@ int main(int argc, char ** argv) {
 
   // input parameters (basic block size, memory length)
 
-  init_allocator(4, 4*1000000);
+  init_allocator(4, 4*100000);
 
   ackerman_main();
 
